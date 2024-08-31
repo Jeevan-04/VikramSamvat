ab5534cbacd4c1d53b250313accac905a3819403
Jeevan Naidu <142775323+Jeevan-04@users.noreply.github.com>
2024-08-31 09:57:35 +0530

    Delete update_date.py

--- update_date.py
+++
@@ -1,9 +0,0 @@
-from app import get_vikram_samvat_date
-
-def update_date_data():
-    # Call the function to get and process the date data
-    data = get_vikram_samvat_date()
-    print(data)
-
-if __name__ == "__main__":
-    update_date_data()
