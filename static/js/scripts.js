742a2f78439072b9dfa18030f76fdcaa87e4b93d
Jeevan Naidu <142775323+Jeevan-04@users.noreply.github.com>
2024-09-01 01:47:57 +0530

    Update scripts.js

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,12 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(response => response.json())
         .then(data => {
             // Access the date parts from the response
-            const dateParts = data.parts;
+            const dateParts = data.lines;
 
             // Display the date parts in respective divs
             document.getElementById('line1').textContent = dateParts[0] || "Date not found";
             document.getElementById('line2').textContent = dateParts[1] || "Date not found";
             document.getElementById('line3').textContent = dateParts[2] || "Date not found";
+            document.getElementById('line4').textContent = dateParts[3] || "Location not found";
         })
         .catch(error => {
             console.error('Error fetching date:', error);
