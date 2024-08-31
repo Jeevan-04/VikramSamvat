3fd6f55dff3f07abe1c8d31e86457242e02ef5b0
Jeevan Naidu <142775323+Jeevan-04@users.noreply.github.com>
2024-09-01 02:01:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,14 @@
+from flask import Flask, render_template, jsonify
+import requests
+from bs4 import BeautifulSoup
+import os
+import logging
+
+app = Flask(__name__)
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+
 def get_vikram_samvat_date():
     url = 'https://www.drikpanchang.com/?geoname-id=1275339'
     response = requests.get(url)
@@ -43,3 +54,16 @@ def get_vikram_samvat_date():
     return {
         'lines': formatted_date_lines
     }
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/api/vikram-samvat-date', methods=['GET'])
+def get_vikram_samvat():
+    date = get_vikram_samvat_date()
+    return jsonify(date)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Default to port 5000 if no PORT environment variable
+    app.run(host="0.0.0.0", port=port, debug=True)
